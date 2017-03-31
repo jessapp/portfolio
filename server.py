e8a30b3b7ceb21e91cf52eea32cfcd58d568aae7
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-03-30 21:33:23 -0700

    fixed PORT for deployment

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 """Portfolio site"""
 
+import os
+
 from jinja2 import StrictUndefined
 
 from flask import (Flask, jsonify, render_template, redirect, request, flash,
@@ -26,17 +28,8 @@ def index():
     return render_template("index.html")
 
 
-if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the
-    # point that we invoke the DebugToolbarExtension
-    app.debug = True
-    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+if __name__ == '__main__':
 
-    # connect_to_db(app)
+    PORT = int(os.environ.get("PORT", 5000))
 
-    # Use the DebugToolbar
-    # DebugToolbarExtension(app)
-
-
-    
-    app.run(port=5000, host='0.0.0.0')
+    app.run(host="0.0.0.0", port=PORT)
