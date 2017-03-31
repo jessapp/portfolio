8e0b74919065e6c1165c1e24b5573a9728db667b
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-03-30 17:06:05 -0700

    Added navbar and formatting

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     app.debug = True
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
-    connect_to_db(app)
+    # connect_to_db(app)
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
