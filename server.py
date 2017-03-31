d2d3198477aec98e6a9548eb9f9b01309d7d278b
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-03-31 11:54:08 -0700

    Added favicon

--- server.py
+++ server.py
@@ -27,6 +27,11 @@ def index():
 
     return render_template("index.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'img'),
+                               'favicon.ico', mimetype='image/png')
+
 
 if __name__ == '__main__':
 
